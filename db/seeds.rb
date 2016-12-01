4433d7904600518da3332fe635399f6fe21461c4
agonzalez0515 <agonzalez0515@gmail.com>
2016-12-01 11:04:13 -0800

    seeds database

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+rory = Character.create(name: "Rory")
+rory.quotes.create(quote: "I can't believe I'm in the blogosphere")
+rory.quotes.create(quote: "I don't know the first thing about having a job")
+rory.quotes.create(quote: "I'm gonna have to quit drinking coffee! And I love coffee! I really love coffee!")
+
+lorelai = Character.create(name: "Lorelai")
+lorelai.quotes.create(quote: "Jesus, Mary, Joseph, and the camel! This is really bad coffee")
+lorelai.quotes.create(quote: "If it was physically possible to make love to a hot beverage, this would be the one.")
+lorelai.quotes.create(quote: "I can be flexible. As long as everything is exactly the way I want it, I'm totally flexible")
+
+michele = Character.create(name: "Michele")
+michele.quotes.create(quote: "People are particularly stupid today. I can't talk to any more of them")
+
+sookie = Character.create(name: "Sookie")
+sookie.quotes.create(quote: "Am I crying or laughing?")
+
+luke = Character.create(name: "Luke")
+luke.quotes.create(quote: "This thing we're doing here, you, me. I'm in. I am all in")
+
+emily = Character.create(name: "Emily")
+emily.quotes.create(quote:"You can use your mother's old gold clubs. They're upstairs, gathering dust, along with the rest of her potential.")
